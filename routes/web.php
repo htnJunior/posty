9ab98c88ed2e8f02125c8f19c08cb3dc46b76734
htnJunior <junioor_souzaa282@hotmail.com>
2021-10-12 19:59:00 -0300

    Primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+
+Route::get('/register', [
+    RegisterController::class, 'index'
+]);
+
+Route::get('/posts', function () {
+    return view('posts.index');
+});
